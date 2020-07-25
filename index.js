2442e3378ea533aa9754347b0e166d58cedcc452
wkk <wenkunkun@foxmail.com>
2020-07-25 11:21:01 +0800

    Revert "添加打包工具"
    This reverts commit 403edb1607febbcf267a468814883ea1e953fd3a.

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-import debounce from './core/debounce'
-import deepClone from './core/deepClone'
-import isEmpty from './core/isEmpty'
-import isEmptyObject from './core/isEmptyObject'
-import isPlainObject from './core/isPlainObject'
-import storage from './core/storage'
-import throttle from './core/throttle'
+import index from ''
 
-export default {
-  debounce,
-  deepClone,
-  isEmpty,
-  isEmptyObject,
-  isPlainObject,
-  storage,
-  throttle
+export {
+
 }

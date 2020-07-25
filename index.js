403edb1607febbcf267a468814883ea1e953fd3a
wkk <wenkunkun@foxmail.com>
2020-07-25 11:14:46 +0800

    添加打包工具

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import index from ''
+import debounce from './core/debounce'
+import deepClone from './core/deepClone'
+import isEmpty from './core/isEmpty'
+import isEmptyObject from './core/isEmptyObject'
+import isPlainObject from './core/isPlainObject'
+import storage from './core/storage'
+import throttle from './core/throttle'
 
-export {
-
+export default {
+  debounce,
+  deepClone,
+  isEmpty,
+  isEmptyObject,
+  isPlainObject,
+  storage,
+  throttle
 }
